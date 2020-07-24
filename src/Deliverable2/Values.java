4bd76db38e283d457bd9cf2f3bbbd97375e16025
antes <antes@DESKTOP-3IB19NG>
2020-07-24 05:42:31 -0400

    made changes for deliverable2

---
+++ src/Deliverable2/Values.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Deliverable2;
+
+
+/**
+ *
+ * @author antesh
+ */
+public enum Values {
+    
+    One , Two , Three , Four , Five , Six , Seven , Eight , Nine , Ten
+    
+}
